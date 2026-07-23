c2d0ac951eb73d326ab995e390d3c49f4eb718be
eduardopdev <eduardohenriquepcs@gmail.com>
2026-07-23 17:48:54 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,15 +5,19 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_yYrnvEi43sCH@ep-cool-waterfall-acjmf33j.sa-east-1.aws.neon.tech/neondb?sslmode=require';
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString,
-  ssl: { rejectUnauthorized: false }
+    connectionString,
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.on('connect', () => {
-  console.log('Conectado ao PostgreSQL com sucesso!');
+    console.log('Conectado ao PostgreSQL com sucesso!');
+});
+
+pool.on('error', (err) => {
+    console.error('Erro inesperado no cliente do PostgreSQL:', err);
 });
 
 export default pool;
